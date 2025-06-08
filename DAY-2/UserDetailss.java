ce7623f76eb069984bc49a19d90baaf909413452
Narmathashree12 <2326kb29@kgcas.com>
2025-06-08 11:59:57 +0530

    Update UserDetailss.java

--- DAY-2/UserDetailss.java
+++ DAY-2/UserDetailss.java
@@ -4,6 +4,6 @@ public static void Details(String name,String Job,String Package) {
     System.out.println("Hello"+" "+name+" "+Job+" "+Package);
 }
 public static void main(String[] args){
-    Details("Abinaya","Developer","6LPA");
+    Details("Narmathashree","Developer","7LPA");
 }
 }
